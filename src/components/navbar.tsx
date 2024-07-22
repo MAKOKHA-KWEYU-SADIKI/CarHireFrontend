36f5f7145aa8de6c7a95f187bc9a2673fdfbe496
MAKOKHA-KWEYU-SADIKI <makokha.14460@students.kyu.ac.ke>
2024-07-23 02:02:10 +0300

    correction in routing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: React.FC = () => {
           <a href="http://localhost:5173/aboutUS" className="text-gray-300 hover:text-white flex items-center">
             {/* <User className="mr-1" />  */}About Us
           </a>
-          <a href="http://localhost:5173/contact" className="text-gray-300 hover:text-white flex items-center">
+          <a href="https://vehiclehiremanagement-system.onrender.com/contact" className="text-gray-300 hover:text-white flex items-center">
             <User className="mr-1" /> Contact us
           </a>
         </div>
